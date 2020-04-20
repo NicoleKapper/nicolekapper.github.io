21c46e0c27777afcdab771e6313e74d6cdb7594c
nicolekapper <nicolekapper99@gmail. com>
2020-04-20 18:36:37 -0400

    isolation

--- projects/isolation/main.js
+++ projects/isolation/main.js
@@ -1,13 +1,20 @@
 
+$(document).ready(function() {
+  console.log("jQuery is ready to use");
+  
+var mybutton = document.getElementById("myBtn");
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-
-
-$("small-title").click(function(){
-  $("small-title").toggleClass("one");
-  $("small-title").html("I like <b>Green</b>!");
-});
-
-document.getElementById("small-title").addEventListener("click", function(){
-  document.getElementById("img").innerHTML = "Hello World";
-});
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
